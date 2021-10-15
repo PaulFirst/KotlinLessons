ee6e1211563e72e18b1dee80ac4832551b36449f
Pavel Maslov <pavelm320@gmail.com>
2021-10-15 21:19:04 +0700

    code for classes is written

--- Classes/src/main/kotlin/Main.kt
+++ Classes/src/main/kotlin/Main.kt
@@ -1,6 +1,82 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+open class Animal
+{
+    open val image = ""
+    open val food = ""
+    open val habitat = ""
+    var hunger = 10
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+    open fun makeNoise()
+    {
+        println("The Animal is making a noise")
+    }
+
+    open fun eat()
+    {
+        println("The Animal is eating")
+    }
+
+    open fun roam()
+    {
+        println("The Animal is roaming")
+    }
+
+    fun sleep() {
+        println("The Animal is sleeping")
+    }
+}
+
+class Hippo: Animal()
+{
+    override val image = "hippo.jpg"
+    override val food = "grass"
+    override val habitat = "water"
+
+    override fun makeNoise()
+    {
+        println("Grunt! Grunt!")
+    }
+}
+
+open class Canine: Animal()
+{
+    override fun roam()
+    {
+        println("The Canine is roaming")
+    }
+}
+
+class Wolf: Canine()
+{
+    override val image = "wolf.jpg"
+    override val food = "meat"
+    override val habitat = "forests"
+
+    override fun makeNoise() {
+        println("Hoooowl!")
+    }
+
+    override fun eat() {
+        println("The Wolf is eating $food")
+    }
+}
+
+class Vet
+{
+    fun giveShot(animal: Animal)
+    {
+        animal.makeNoise()
+    }
+}
+
+fun main() {
+    val animals = arrayOf(Hippo(), Wolf())
+    for (item in animals)
+    {
+        item.roam()
+        item.eat()
+    }
+
+    val vet = Vet()
+    vet.giveShot(animals[1])
+    vet.giveShot(animals[0])
 }
